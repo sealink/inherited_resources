fa0e0a9b0b688866b88949105c00d2b836af959f
Tony Levi <tony.levi@sealink.com.au>
2019-07-29 15:14:16 +0930

    Allow override of the generated method names

--- app/controllers/inherited_resources/base.rb
+++ app/controllers/inherited_resources/base.rb
@@ -23,11 +23,6 @@ module InheritedResources
 
         helper_method :resource, :collection, :resource_class, :association_chain,
                       :resource_instance_name, :resource_collection_name,
-                      :resource_url, :resource_path,
-                      :collection_url, :collection_path,
-                      :new_resource_url, :new_resource_path,
-                      :edit_resource_url, :edit_resource_path,
-                      :parent_url, :parent_path,
                       :smart_resource_url, :smart_collection_url
 
         self.class_attribute :resource_class, instance_writer: false unless self.respond_to? :resource_class
